72c31ed144dc7dab1493797b81788af0f107f78b
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2021-03-10 21:36:11 +0200

    add Stepper with customized states

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatStepperModule} from '@angular/material/stepper';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [
@@ -59,6 +60,7 @@ import { MatInputModule} from '@angular/material/input';
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontEnd/src/app/pages/booking/booking.component.ts
+++ frontEnd/src/app/pages/booking/booking.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
-  styleUrls: ['./booking.component.less']
+  styleUrls: ['./booking.component.less'],
+  providers: [{
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: {displayDefaultIndicatorType: false}
+  }]
 })
 export class BookingComponent implements OnInit {
 
