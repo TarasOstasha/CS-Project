02438aa716f721f9464f481d64af565db533215e
TarasOstasha <tonyjoss1990@gmail.com>
2021-03-11 17:46:59 -0500

    footer done

--- frontEnd/src/app/parts/footer/footer.component.ts
+++ frontEnd/src/app/parts/footer/footer.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.less']
 })
 export class FooterComponent implements OnInit {
+  serviceCards = [
+    { name: 'Regular Cleaning', routerLink: ['/regular-cleaning'], img: '../../../assets/images/services/regular-cleaning.jpg' },
+    { name: 'Deep Cleaning', routerLink: ['/deep-cleaning'], img: '../../../assets/images/services/deep-cleaning.jpg' },
+    { name: 'Move Cleaning ', routerLink: ['/move-cleaning'], img: '../../../assets/images/services/move-cleaning.jpg' },
+    { name: 'Post Construction Cleaning', routerLink: ['/post-construction'], img: '../../../assets/images/services/post-construction.jpg' },
+    { name: 'Post Renovation Cleaning ', routerLink: ['/post-renovation'], img: '../../../assets/images/services/post-construction.jpg' },
+    { name: 'Office Cleaning ', routerLink: ['/office-cleaning'], img: '../../../assets/images/services/office-cleaning.jpg' }
+  ];
 
   constructor() { }
 
