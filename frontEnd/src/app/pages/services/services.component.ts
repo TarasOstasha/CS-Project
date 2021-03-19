f5bc50b7ee5d8cfa1b2e9fda2b4d42bdbbd6220d
TarasOstasha <tonyjoss1990@gmail.com>
2021-03-18 21:40:38 -0400

    page services done

--- frontEnd/src/app/pages/services/services.component.ts
+++ frontEnd/src/app/pages/services/services.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.less']
 })
 export class ServicesComponent implements OnInit {
+  public path = '/services';
+  serviceCards = [
+    { name: 'regular cleaning', routerLink: ['/regular-cleaning'], img: '../../../assets/images/services/regular-cleaning.jpg' },
+    { name: 'Deep Cleaning', routerLink: ['/deep-cleaning'], img: '../../../assets/images/services/deep-cleaning.jpg' },
+    { name: 'move Cleaning ', routerLink: ['/move-cleaning'], img: '../../../assets/images/services/move-cleaning.jpg' },
+    { name: 'Post Construction Cleaning', routerLink: ['/post-construction'], img: '../../../assets/images/services/post-construction1.jpg' },
+    { name: 'post renovation cleaning ', routerLink: ['/post-renovation'], img: '../../../assets/images/services/post-renovation.jpg' },
+    { name: 'Office Cleaning ', routerLink: ['/office-cleaning'], img: '../../../assets/images/services/office-cleaning.jpg' }
+  ];
 
   constructor() { }
 
