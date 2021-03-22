c5c9168a92bf0cb4b167c6086cae5227a9ad9d9e
TarasOstasha <tonyjoss1990@gmail.com>
2021-03-21 20:06:01 -0400

    added btb book on all services page

--- frontEnd/src/app/pages/service-pages/meve/meve.component.ts
+++ frontEnd/src/app/pages/service-pages/meve/meve.component.ts
@@ -106,6 +106,7 @@ export class MeveComponent implements OnInit {
   serviceCards = [
     { name: 'regular cleaning', routerLink: ['/regular-cleaning'], img: '../../../assets/images/services/regular-cleaning.jpg' },
     { name: 'Deep Cleaning', routerLink: ['/deep-cleaning'], img: '../../../assets/images/services/deep-cleaning.jpg' },
+    { name: 'Organic Cleaning', routerLink: ['/green-cleaning'], img: '../../../assets/images/services/green-cleaning.jpg' },
     { name: 'move Cleaning ', routerLink: ['/move-cleaning'], img: '../../../assets/images/services/move-cleaning.jpg' },
     { name: 'Post Construction Cleaning', routerLink: ['/post-construction'], img: '../../../assets/images/services/post-construction1.jpg' },
     { name: 'post renovation cleaning ', routerLink: ['/post-renovation'], img: '../../../assets/images/services/post-renovation.jpg' },
