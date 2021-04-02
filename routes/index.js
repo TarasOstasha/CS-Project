543f7d83e0b2ede9273f84b80b5cab6044a7aad3
TarasOstasha <tonyjoss1990@gmail.com>
2021-04-02 16:22:47 -0400

    added fs

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+const fs = require('fs');
+const pfs = fs.promises;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
