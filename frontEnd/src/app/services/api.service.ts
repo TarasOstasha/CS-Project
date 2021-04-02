f272ed6587a27f02914c5d93cc0aaadd602c661f
TarasOstasha <tonyjoss1990@gmail.com>
2021-04-02 16:46:33 -0400

    fixed url

--- frontEnd/src/app/services/api.service.ts
+++ frontEnd/src/app/services/api.service.ts
@@ -9,7 +9,8 @@ import { Review } from '../interfaces/review-data.model';
   providedIn: 'root'
 })
 export class ApiService {
-  private url = 'http://localhost:3000/';
+  //private url = 'http://localhost:3000/';
+  private url = '';
 
 
   constructor(private _http: HttpClient) { }
