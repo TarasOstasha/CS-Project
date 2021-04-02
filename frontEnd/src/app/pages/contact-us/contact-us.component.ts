9c5ece8c0e409358feeda04cd96a6b520081198f
TarasOstasha <tonyjoss1990@gmail.com>
2021-04-01 20:27:13 -0400

    added about us page

--- frontEnd/src/app/pages/contact-us/contact-us.component.ts
+++ frontEnd/src/app/pages/contact-us/contact-us.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl, NgForm } from '@angular/forms';
+
+import { ApiService } from '../../services/api.service';
+import { pipe, Subject, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+declare var $: any;
+declare var jQuery: any;
+
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +17,111 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.less']
 })
 export class ContactUsComponent implements OnInit {
+  experienceForm!: FormGroup;
+  @Input() starRating: any;
+  @Input() starWidth!: number;
+  rating!: number;
+  clickedBtn: boolean = false;
+  panelOpenState: boolean = false;
+  public myReviewsArr: any = [];
+  public reviews = [
+    { title: ' "Lorem ipsum dolor1..." ', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis voluptas odit consectetur necessitatibus suscipit libero nam porro hic, cumque quos, aspernatur voluptatem laboriosam modi voluptatum nesciunt deleniti repellat error doloribus! Lorem ipsum dolor sit amet, consectetur adipisicing elit.', name: 'Jack', rating: 4 },
+    { title: ' "Lorem ipsum dolor2..." ', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis voluptas odit consectetur necessitatibus suscipit libero nam porro hic, cumque quos, aspernatur voluptatem laboriosam modi voluptatum nesciunt deleniti repellat error doloribus! Lorem ipsum dolor sit amet, consectetur adipisicing elit.', name: 'Jack', rating: 3 },
+    { title: ' "Lorem ipsum dolor3..." ', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis voluptas odit consectetur necessitatibus suscipit libero nam porro hic, cumque quos, aspernatur voluptatem laboriosam modi voluptatum nesciunt deleniti repellat error doloribus! Lorem ipsum dolor sit amet, consectetur adipisicing elit.', name: 'Jack', rating: 4 },
+    { title: ' "Lorem ipsum dolor4..." ', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis voluptas odit consectetur necessitatibus suscipit libero nam porro hic, cumque quos, aspernatur voluptatem laboriosam modi voluptatum nesciunt deleniti repellat error doloribus! Lorem ipsum dolor sit amet, consectetur adipisicing elit.', name: 'Jack', rating: 3 }
+  ];
 
-  constructor() { }
+  experiences = [
+    { value: 'Excellent' },
+    { value: 'Good' },
+    { value: 'Acceptable' },
+    { value: 'Bad' }
+  ];
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _api: ApiService,
+    private _snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.initReviewsCarousel();
+    }, 500)
+    this.experienceForm = this._formBuilder.group({
+      experience: ['', Validators.required],
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      myTextArea: ['', [Validators.required, Validators.minLength(20)]]
+    });
+    this.getMyReview();
   }
 
+  getMyReview() {
+    this._api.getReviews()
+      .pipe(
+        map((review: any) => {
+          return review.reviews;
+        })
+      )
+      .subscribe((response) => {
+        this.myReviewsArr = response;
+        //console.log(this.myReviewsArr)
+      })
+  }
+
+  initReviewsCarousel() {
+    $('.slider').slick({
+      infinite: true,
+      autoplay: true,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      autoplaySpeed: 5000,
+      mobileFirst: true,
+      adaptiveHeight: true
+    });
+  }
+
+  selectedExperience() {
+
+  }
+
+  onRatingClicked(message: any): void {
+    this.starRating = message
+    this.clickedBtn = true;
+    //rconsole.log(this.starRating)
+  }
+
+  checkedStar(icon: any) {
+    this.starWidth = icon * 118 / 5;
+    //console.log(icon)
+    //console.log(this.starWidth, 'px')
+  }
+  saveReview() {
+    //console.log(this.experienceForm.value)
+    if (this.experienceForm.invalid && this.experienceForm.value.experience == null || this.experienceForm.value.name == null || this.experienceForm.value.myTextArea == null) {
+       this.clickedBtn = false; // after sent review make btn send disabled
+    } else if(this.experienceForm.valid) {
+      this.clickedBtn = true; // if form valid make a enabled btn send to send new form
+    }
+    this._api.sendReview(this.experienceForm.value.experience, this.experienceForm.value.name, this.experienceForm.value.myTextArea, this.starRating);
+    //this.getMyReview(); // call immediately new reviews but not working because of slick jquery carousel initialize bad
+    this.panelOpenState = false; // close review window after submit
+    this.experienceForm.reset(); // reset form
+    Object.keys(this.experienceForm.controls).forEach(key =>{
+      this.experienceForm.controls[key].setErrors(null); // set all value null after submit
+    });  
+    this.openSnackBar('Review was sent', 'Thank you!'); // show popup message after submit
+    this.clickedBtn = false; // move send btn to disabled after submit
+  }
+
+  // popup menu after submitted form
+  openSnackBar(message: string, action: any) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+
+  openPanel() {
+    this.panelOpenState = true;
+  }
 }
--- frontEnd/src/app/pages/pricing/pricing.component.ts
+++ frontEnd/src/app/pages/pricing/pricing.component.ts
@@ -17,7 +17,7 @@ export class PricingComponent implements OnInit {
         { type: '3 bedroom/1 bathroom', price: '150$', frequency1: 'Weekly', frequency2: 'Biweekly - 160$', frequency3: 'Monthly - 170$', frequency4: 'One time - 175$',  rec_time: 'Recommend time - 4h' },
         { type: '3 bedroom/2 bathroom', price: '180$', frequency1: 'Weekly', frequency2: 'Biweekly - 190$', frequency3: 'Monthly - 200$', frequency4: 'One time - 205$',  rec_time: 'Recommend time - 5h' },
         { type: 'Hourly rate', price: '1 cleaner - 40$/h', frequency1: '2 cleaners - 75$/h', rec_time: '3 h minimum' },
-        { type: 'Extra services', price: 'Refrigerator - $30', frequency1: 'Oven - $30', frequency2: 'inside cabinets - $30', frequency3: 'inside windows - $30', frequency4: 'vacuum the sofa - $30',  rec_time: 'Laundry wash/dry - $30' }
+        { type: 'Extra services', extraService1: 'Refrigerator - $30', extraService2: 'Oven - $30', extraService3: 'inside cabinets - $30', extraService4: 'inside windows - $30', extraService5: 'vacuum the sofa - $30',  rec_time: 'Laundry wash/dry - $30' }
       ]
   }
 
