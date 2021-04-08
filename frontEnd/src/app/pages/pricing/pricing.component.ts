331f5e01fc2c5d8e73749442fbe1b3833254b3f6
TarasOstasha <tonyjoss1990@gmail.com>
2021-04-08 19:55:40 -0400

    changes done

--- frontEnd/src/app/pages/pricing/pricing.component.ts
+++ frontEnd/src/app/pages/pricing/pricing.component.ts
@@ -16,8 +16,8 @@ export class PricingComponent implements OnInit {
         { type: '2 bedroom/2 bathroom', price: '160$', frequency1: 'Weekly', frequency2: 'Biweekly - 170$', frequency3: 'Monthly - 180$', frequency4: 'One time - 185$',  rec_time: 'Recommend time - 4,5h' },
         { type: '3 bedroom/1 bathroom', price: '150$', frequency1: 'Weekly', frequency2: 'Biweekly - 160$', frequency3: 'Monthly - 170$', frequency4: 'One time - 175$',  rec_time: 'Recommend time - 4h' },
         { type: '3 bedroom/2 bathroom', price: '180$', frequency1: 'Weekly', frequency2: 'Biweekly - 190$', frequency3: 'Monthly - 200$', frequency4: 'One time - 205$',  rec_time: 'Recommend time - 5h' },
-        { type: 'Hourly rate', price: '1 cleaner - 40$/h', frequency1: '2 cleaners - 75$/h', rec_time: '3 h minimum' },
-        { type: 'Extra services', extraService1: 'Refrigerator - $30', extraService2: 'Oven - $30', extraService3: 'inside cabinets - $30', extraService4: 'inside windows - $30', extraService5: 'vacuum the sofa - $30',  rec_time: 'Laundry wash/dry - $30' }
+        { type: 'Hourly rate', cleaners: '1 cleaner - 40$/h', cleaners2: '2 cleaners - 75$/h', recommended_time: '3 h minimum' },
+        { type: 'Extra services', extraService1: 'Refrigerator  $30', extraService2: 'Oven  $30', extraService3: 'Inside cabinets  $30', extraService4: 'Inside windows  $30', extraService5: 'Vacuum the sofa  $30',  rec_time: 'Laundry wash/dry  $30' }
       ]
   }
 
